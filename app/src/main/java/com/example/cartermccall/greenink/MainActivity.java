22a4480ae8d1ea1a082323ef188d8e3fd7aa851e
Carter McCall <cartermccall@Carters-MacBook-Air.local>
2018-11-15 10:31:31 -0600

    App Template

--- app/src/main/java/com/example/cartermccall/greenink/MainActivity.java
+++ app/src/main/java/com/example/cartermccall/greenink/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.cartermccall.greenink;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button tutorialButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tutorialButton = (Button) findViewById(R.id.tutorial_button);
+        tutorialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), TutorialActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
